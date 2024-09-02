625218d32969ae18b2de222c5dc7a86f3f63e153
Philip Hell <blvckbytes@gmail.com>
2024-09-02 12:03:42 +0200

    removed ui

--- src/main/java/me/blvckbytes/storage_query/StorageQueryPlugin.java
+++ src/main/java/me/blvckbytes/storage_query/StorageQueryPlugin.java
@@ -1,19 +1,14 @@
 package me.blvckbytes.storage_query;
 
 import me.blvckbytes.storage_query.translation.*;
-import me.blvckbytes.storage_query.ui.ResultDisplayHandler;
-import me.blvckbytes.storage_query.ui.StorageQueryCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.Registry;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public class StorageQueryPlugin extends JavaPlugin {
-
-  private ResultDisplayHandler displayHandler = null;
 
   @Override
   public void onEnable() {
@@ -45,25 +40,7 @@ public class StorageQueryPlugin extends JavaPlugin {
     var registryGerman = TranslationRegistry.load("/de_de.json", germanSources, logger);
     var registryEnglish = TranslationRegistry.load("/en_us.json", englishSources, logger);
 
-    if (registryGerman == null || registryEnglish == null) {
+    if (registryGerman == null || registryEnglish == null)
       Bukkit.getPluginManager().disablePlugin(this);
-      return;
-    }
-
-    displayHandler = new ResultDisplayHandler(this);
-
-    getServer().getPluginManager().registerEvents(displayHandler, this);
-
-    var commandExecutor = new StorageQueryCommand(registryGerman, registryEnglish, displayHandler);
-    var pluginCommand = Objects.requireNonNull(getCommand("lagersuche"));
-
-    pluginCommand.setExecutor(commandExecutor);
-    pluginCommand.setTabCompleter(commandExecutor);
-  }
-
-  @Override
-  public void onDisable() {
-    if (this.displayHandler != null)
-      displayHandler.onShutdown();
   }
 }
